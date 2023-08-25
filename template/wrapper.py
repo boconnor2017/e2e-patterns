a0cfd8a895217c5bd50847360607d57a4dc555f5
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-25 18:15:36 -0400

    Update wrapper.py

--- template/wrapper.py
+++ template/wrapper.py
@@ -7,3 +7,22 @@
 # Add python script (example below)
 a = "This works!!"
 print(a)
+
+# Get pattern config file
+src_file = '/usr/local/e2e-patterns/config.py'
+des_dir = os.getcwd()
+print("Retrieving config file from "+src_file+" and copying to "+des_dir)
+outpt = shutil.copy(src_file, des_dir)
+print(str(outpt))
+print("")
+
+# Get pattern library
+src_file = '/usr/local/e2e-k8-lab/lib.py'
+des_dir = os.getcwd()
+print("Retrieving library file from "+src_file+" and copying to "+des_dir)
+outpt = shutil.copy(src_file, des_dir)
+print(str(outpt))
+print("")
+
+import config
+import lib
