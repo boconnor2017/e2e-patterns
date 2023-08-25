bb493f1307bbc232eb9b0c801f0cc20492bd4818
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-25 18:09:57 -0400

    Create wrapper.py

---
+++ template/wrapper.py
@@ -0,0 +1,9 @@
+# Wrapper files should follow this naming convention: wrapper-build-e2e-pattern-<NAME>.py
+# Copy Template Below
+# # # # # # # # # # # # # # # # # # # # # # # # #
+# <DESCRIPTION>
+# Author: Brendan O'Connor
+# Date: <MONTH YEAR>
+# Add python script (example below)
+a = "This works!!"
+print(a)
