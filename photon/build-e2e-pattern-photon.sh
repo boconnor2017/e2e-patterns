e502915066718bd00c9cfd2b5db49909395342df
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-26 10:46:34 -0400

    Update build-e2e-pattern-photon.sh

--- photon/build-e2e-pattern-photon.sh
+++ photon/build-e2e-pattern-photon.sh
@@ -1,1 +1,8 @@
-#TODO
+Builds a PhotonOS VM
+# Author: Brendan O'Connor
+# Date: August 2023
+
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip setuptools
+python3 wrapper.py wrapper-build-e2e-pattern-photon.py
