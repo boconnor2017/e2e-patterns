b35b2cabd5cdeb97d3c257a739397d78cf86f718
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-26 21:21:40 -0400

    Update configure-e2e-pattern-photon.sh

--- photon/configure-e2e-pattern-photon.sh
+++ photon/configure-e2e-pattern-photon.sh
@@ -1,4 +1,4 @@
 # Configures Photon using PowerCLI container
 # Author: Brendan O'Connor
 # Date: August 2023
-docker run --rm --entrypoint="/usr/bin/pwsh" -v ${PWD}:/tmp vmware/powerclicore /tmp/wrapper-configure-e2e-pattern.ps1 "$1 $2 $3"
+docker run --rm --entrypoint="/usr/bin/pwsh" -v ${PWD}:/tmp vmware/powerclicore /tmp/wrapper-configure-e2e-pattern-photon.ps1 "$1 $2 $3"
