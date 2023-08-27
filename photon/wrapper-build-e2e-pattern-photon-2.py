3866e6b32bc2fa008a3a83e47b06e61edc698e5e
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-26 23:26:57 -0400

    Update wrapper-build-e2e-pattern-photon-2.py

--- photon/wrapper-build-e2e-pattern-photon-2.py
+++ photon/wrapper-build-e2e-pattern-photon-2.py
@@ -34,15 +34,31 @@ pcli_source = "configure-photon.ps1"
 err = "powercli source: "+pcli_source
 lib.write_to_logs(err, logfile_name)
 
-
 # Run PowerCLI container 
-# docker run --rm --entrypoint="/usr/bin/pwsh" -v ${PWD}:/tmp vmware/powerclicore /tmp/configure-photon.ps1 "172.16.0.201 root VMware1! root VMware1!VMware1!" 
-
 docker_rm = True 
 docker_entrypoint = "/usr/bin/pwsh"
 docker_volume = {os.getcwd():{'bind':'/tmp', 'mode':'rw'}}
 docker_image = "vmware/powerclicore"
-docker_cmd = "/tmp/configure-photon.ps1 \"172.16.0.201 root VMware1! root VMware1!VMware1!\""
+docker_cmd = "/tmp/configure-photon.ps1 \""+config.E2EP_ENVIRONMENT().esxi_host_ip+" "+config.E2EP_ENVIRONMENT().esxi_host_username+" "+config.E2EP_ENVIRONMENT().esxi_host_password+" "+config.E2EP_ENVIRONMENT().photonos_username+" "+config.E2EP_ENVIRONMENT().photonos_password+"\""
+
+err = "docker run parameters:"
+lib.write_to_logs(err, logfile_name)
+err = "    docker_image: "+docker_image
+lib.write_to_logs(err, logfile_name)
+err = "    docker_rm: "+str(docker_rm)
+lib.write_to_logs(err, logfile_name)
+err = "    docker_entrypoint: "+docker_entrypoint
+lib.write_to_logs(err, logfile_name)
+err = "    docker_volumes: "+str(docker_volume)
+lib.write_to_logs(err, logfile_name)
+err = "    docker_cmd: "+docker_cmd
+lib.write_to_logs(err, logfile_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
+err = "executing docker run command..."
+lib.write_to_logs(err, logfile_name)
+
 client = docker.from_env()
 client.containers.run(image=docker_image, entrypoint=docker_entrypoint, volumes=docker_volume, remove=docker_rm, command=docker_cmd)
-
+err = "Finished! If the results are different than expected, troubleshoot the ps1 script directly."
+lib.write_to_logs(err, logfile_name)
