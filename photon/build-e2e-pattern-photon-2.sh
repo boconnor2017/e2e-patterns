f22dc7807353ecfa1c8ca119d98ca9b2662511a6
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-28 14:00:02 -0400

    Update build-e2e-pattern-photon-2.sh

--- photon/build-e2e-pattern-photon-2.sh
+++ photon/build-e2e-pattern-photon-2.sh
@@ -1,9 +1,8 @@
-# Configures Photon using PowerCLI container
+# Runs proton prep script remotely from master controller
 # Author: Brendan O'Connor
 # Date: August 2023
 python3 -m ensurepip
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pip setuptools
-python3 -m pip install docker
 python3 -m pip install paramiko
 python3 wrapper-build-e2e-pattern-photon-2.py
