7c7a4226872d3b462ee1d1c7c2611778578adf88
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-28 13:59:24 -0400

    Update wrapper-build-e2e-pattern-photon-2.py

--- photon/wrapper-build-e2e-pattern-photon-2.py
+++ photon/wrapper-build-e2e-pattern-photon-2.py
@@ -1,9 +1,9 @@
-# Configure PhotonOS using PowerCLI container
+# Run photon prep script remotely from master controller
 # Author: Brendan O'Connor
 # Date: August 2023
 import os
 import shutil
-import docker
+import paramiko
 
 # Get pattern config file
 src_file = '/usr/local/e2e-patterns/config.py'
@@ -26,39 +26,99 @@ import lib
 
 # Start log file
 logfile_name = config.LOGS().photonos
-err = "Starting wrapper-build-e2e-pattern-photon-2.py"
-lib.write_to_logs(err, logfile_name)
-
-# Parameters
-pcli_source = "configure-photon.ps1"
-err = "powercli source: "+pcli_source
-lib.write_to_logs(err, logfile_name)
-
-# Run PowerCLI container 
-docker_rm = True 
-docker_entrypoint = "/usr/bin/pwsh"
-docker_volume = {os.getcwd():{'bind':'/tmp', 'mode':'rw'}}
-docker_image = "vmware/powerclicore"
-docker_cmd = "/tmp/configure-photon.ps1 \""+config.E2EP_ENVIRONMENT().esxi_host_ip+" "+config.E2EP_ENVIRONMENT().esxi_host_username+" "+config.E2EP_ENVIRONMENT().esxi_host_password+" "+config.E2EP_ENVIRONMENT().photonos_username+" "+config.E2EP_ENVIRONMENT().photonos_password+"\""
-
-err = "docker run parameters:"
-lib.write_to_logs(err, logfile_name)
-err = "    docker_image: "+docker_image
-lib.write_to_logs(err, logfile_name)
-err = "    docker_rm: "+str(docker_rm)
-lib.write_to_logs(err, logfile_name)
-err = "    docker_entrypoint: "+docker_entrypoint
-lib.write_to_logs(err, logfile_name)
-err = "    docker_volumes: "+str(docker_volume)
-lib.write_to_logs(err, logfile_name)
-err = "    docker_cmd: "+docker_cmd
+err = ""
 lib.write_to_logs(err, logfile_name)
 err = ""
 lib.write_to_logs(err, logfile_name)
-err = "executing docker run command..."
+err = ""
+lib.write_to_logs(err, logfile_name)
+err = "* * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+lib.write_to_logs(err, logfile_name)
+err = "* * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+lib.write_to_logs(err, logfile_name)
+err = "Starting wrapper-build-e2e-pattern-photon-2.py"
 lib.write_to_logs(err, logfile_name)
 
-client = docker.from_env()
-client.containers.run(image=docker_image, entrypoint=docker_entrypoint, volumes=docker_volume, remove=docker_rm, command=docker_cmd)
-err = "Finished! If the results are different than expected, troubleshoot the ps1 script directly."
+def run_ssh_command(run_cmd):
+	i=0
+	for command in run_cmd:
+	    err = "    ["+str(i)+"] "+command
+	    lib.write_to_logs(err, logfile_name)
+	    stdin, stdout, stderr = client.exec_command(command, timeout=None)
+	    err = (stdout.read().decode())
+	    lib.write_to_logs(err, logfile_name)
+	    err = stderr.read().decode()
+	    lib.write_to_logs(err, logfile_name)
+	    #pause_dur = 0
+	    #err = "Pausing python runtime for "+str(pause_dur)+" seconds..."
+	    #lib.write_to_logs(err, logfile_name)
+	    #lib.pause_python_for_duration(pause_dur)
+	    #err = "Resuming python runtime"
+	    #lib.write_to_logs(err, logfile_name)
+	    err = ""
+	    lib.write_to_logs(err, logfile_name)
+	    i=i+1
+
+# Parameters
+photon_host = "172.16.0.128"
+photon_prep_source = "/usr/local/prep-photon.sh"
+photon_refresh_source = "/usr/local/refresh-e2e-patterns.sh" 
+
+# Create commands for PhotonOS
+photon_prep_downloads = [
+    "curl https://raw.githubusercontent.com/boconnor2017/e2e-patterns/main/prep-photon.sh >> /usr/local/prep-photon.sh",
+    "curl https://raw.githubusercontent.com/boconnor2017/e2e-patterns/main/refresh-e2e-patterns.sh >> /usr/local/refresh-e2e-patterns.sh"
+]
+
+with open(photon_prep_source) as file:
+	txt = file.read()
+
+photon_prep_run_script = txt.split('\n')
+
+# Connect to SSH Host
+err = "Connecting to ssh host "+photon_host
+lib.write_to_logs(err, logfile_name)
+
+# Validate commands
+err = "Validating commands:"
+lib.write_to_logs(err, logfile_name)
+i=0
+for command_validate in photon_prep_downloads:
+	err = "    ["+str(i)+"] "+command_validate
+	lib.write_to_logs(err, logfile_name)
+	i=i+1
+
+err = ""
+lib.write_to_logs(err, logfile_name)
+i=0
+for command_validate in photon_prep_run_script:
+	err = "    ["+str(i)+"] "+command_validate
+	lib.write_to_logs(err, logfile_name)
+	i=i+1
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+try:
+    client.connect(hostname=photon_host, username=config.E2EP_ENVIRONMENT().photonos_username, password=config.E2EP_ENVIRONMENT().photonos_password)
+except:
+    print("[!] Cannot connect to the SSH Server")
+    exit()
+
+# Download scripts to PhotonOS
+err = "Downloading scripts to Photon from github:"
+lib.write_to_logs(err, logfile_name)
+run_ssh_command(photon_prep_downloads)
+err = ""
+lib.write_to_logs(err, logfile_name)
+
+# Execute prep scripts to PhotonOS
+err = "Executing photon prep scripts:"
+lib.write_to_logs(err, logfile_name)
+run_ssh_command(photon_prep_run_script)
+err = ""
+lib.write_to_logs(err, logfile_name)
+
+# Close SSH Session
+client.close()
+err = "Finished. SSH Session closed."
 lib.write_to_logs(err, logfile_name)
