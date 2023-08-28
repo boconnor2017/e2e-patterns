a9faac0efd47acb665a121ad060cc2d34df24cfe
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-28 13:56:32 -0400

    Update prep-photon.sh

--- prep-photon.sh
+++ prep-photon.sh
@@ -3,9 +3,8 @@ yum -y install git bindutils ansible
 yum -y install https://repo.ius.io/ius-release-el$(rpm -E '%{rhel}').rpm
 yum -y install python3
 python3 --version
-cd /usr/local
-mkdir drop
-git clone https://github.com/boconnor2017/e2e-k8-lab.git
+mkdir /usr/local/drop
+git clone https://github.com/boconnor2017/e2e-k8-lab.git /usr/local/e2e-k8-lab
 systemctl start docker
 systemctl status docker
 curl https://raw.githubusercontent.com/boconnor2017/e2e-k8-lab/main/dns-server/resolv.conf >> /run/systemd/resolve/new-resolv.conf
