0813d86ee3b9dc29dcfa8b64fbcbf8aa58d25564
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-29 15:15:10 -0400

    Update wrapper-build-e2e-pattern-controller.py

--- photon/wrapper-build-e2e-pattern-controller.py
+++ photon/wrapper-build-e2e-pattern-controller.py
@@ -1,11 +1,17 @@
-# Run photon prep script remotely from master controller
+# Builds a photon controller (a prepped photon appliance ready for building patterns)
+# 	Deploy photon: uses ovftool container
+#	Change password: uses powercli container
+#	Get IP address (DHCP assigned): uses powercli container
+#	Configure photon: uses paramiko to ssh into the os 
 # Author: Brendan O'Connor
 # Date: August 2023
+
 import os
 import shutil
 import paramiko
 import docker
 import subprocess
+import sys
 
 # Get pattern config file
 src_file = '/usr/local/e2e-patterns/config.py'
@@ -41,6 +47,7 @@ lib.write_to_logs(err, logfile_name)
 err = "Starting wrapper-build-e2e-pattern-photon-2.py"
 lib.write_to_logs(err, logfile_name)
 
+# Functions 
 def run_ssh_command(run_cmd):
 	i=0
 	for command in run_cmd:
@@ -55,11 +62,17 @@ def run_ssh_command(run_cmd):
 	    lib.write_to_logs(err, logfile_name)
 	    i=i+1
 
-# Parameters
-photon_host = "172.16.0.136"
-photon_vm_name = "e2ep-photontest-11"
-photon_prep_source = "/usr/local/prep-photon.sh"
-photon_refresh_source = "/usr/local/refresh-e2e-patterns.sh" 
+
+# Virtual Machine Details
+class VM():
+	name = sys.argv[1] # as displayed in vCenter
+	source = sys.argv[2] # (syntax: ova-name.ova)
+	prep_script = "/usr/local/prep-photon.sh"
+	refresh_script = "/usr/local/refresh-e2e-patterns.sh"
+
+
+# Deploy Photon Appliance using ovftool container
+print("TEMPORARY: in the future this will deploy photon.")
 
 # Configure password using powercli container
 err = "Configure password using pcli container:"
@@ -68,13 +81,16 @@ docker_rm = True
 docker_entrypoint = "/usr/bin/pwsh"
 docker_volume = {os.getcwd():{'bind':'/tmp', 'mode':'rw'}}
 docker_image = "vmware/powerclicore"
-docker_cmd = "/tmp/configure-photon.ps1 \""+config.E2EP_ENVIRONMENT().esxi_host_ip+" "+config.E2EP_ENVIRONMENT().esxi_host_username+" "+config.E2EP_ENVIRONMENT().esxi_host_password+" "+photon_vm_name+" "+config.E2EP_ENVIRONMENT().photonos_password+"\""
+docker_cmd = "/tmp/configure-photon.ps1 \""+config.E2EP_ENVIRONMENT().esxi_host_ip+" "+config.E2EP_ENVIRONMENT().esxi_host_username+" "+config.E2EP_ENVIRONMENT().esxi_host_password+" "+VM().name+" "+config.E2EP_ENVIRONMENT().photonos_password+"\""
 err = "    docker_cmd: "+docker_cmd
 lib.write_to_logs(err, logfile_name)
 client = docker.from_env()
 client.containers.run(image=docker_image, entrypoint=docker_entrypoint, volumes=docker_volume, remove=docker_rm, command=docker_cmd)
 err = ""
 lib.write_to_logs(err, logfile_name)
+
+# Get IP Address of the new photon os by name using powercli container
+photon_ip_address = "172.16.0.138"
 
 # Pause to allow password change to take effect 
 seconds = 30
@@ -83,20 +99,22 @@ lib.write_to_logs(err, logfile_name)
 lib.pause_python_for_duration(seconds)
 err = "Resuming script."
 lib.write_to_logs(err, logfile_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
 
-# Create commands for PhotonOS
+# Create commands for PhotonOS paramiko configuration
 photon_prep_downloads = [
-    "curl https://raw.githubusercontent.com/boconnor2017/e2e-patterns/main/prep-photon.sh >> /usr/local/prep-photon.sh",
-    "curl https://raw.githubusercontent.com/boconnor2017/e2e-patterns/main/refresh-e2e-patterns.sh >> /usr/local/refresh-e2e-patterns.sh"
+    "curl https://raw.githubusercontent.com/boconnor2017/e2e-patterns/main/prep-photon.sh >> "+VM().prep_script,
+    "curl https://raw.githubusercontent.com/boconnor2017/e2e-patterns/main/refresh-e2e-patterns.sh >> "+VM().refresh_script
 ]
 
-with open(photon_prep_source) as file:
+with open(VM().prep_script) as file:
 	txt = file.read()
 
 photon_prep_run_script = txt.split('\n')
 
 # Connect to SSH Host
-err = "Connecting to ssh host "+photon_host
+err = "Connecting to ssh host "+photon_ip_address
 lib.write_to_logs(err, logfile_name)
 
 # Validate commands
@@ -119,7 +137,7 @@ for command_validate in photon_prep_run_script:
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 try:
-    client.connect(hostname=photon_host, username=config.E2EP_ENVIRONMENT().photonos_username, password=config.E2EP_ENVIRONMENT().photonos_password)
+    client.connect(hostname=photon_ip_address, username=config.E2EP_ENVIRONMENT().photonos_username, password=config.E2EP_ENVIRONMENT().photonos_password)
 except:
     print("[!] Cannot connect to the SSH Server")
     exit()
