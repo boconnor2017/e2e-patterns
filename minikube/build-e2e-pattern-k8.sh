99d1d4ca15216a4c2d4f1d16f66bf8453d59b83a
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-30 09:46:44 -0400

    Update build-e2e-pattern-k8.sh

--- minikube/build-e2e-pattern-k8.sh
+++ minikube/build-e2e-pattern-k8.sh
@@ -1,1 +1,8 @@
-#TODO
+# Builds Minikube Pattern
+# Author: Brendan O'Connor
+# Date: August 2023
+
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip setuptools
+python3 wrapper-build-e2e-pattern-k8.py $1
