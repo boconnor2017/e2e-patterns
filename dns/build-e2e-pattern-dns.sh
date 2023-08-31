ca3c8f359349efa5eb7d567c189958dff1f77937
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-31 09:02:12 -0400

    Create build-e2e-pattern-dns.sh

---
+++ dns/build-e2e-pattern-dns.sh
@@ -0,0 +1,8 @@
+# Builds DNS Server using Tanium container
+# Author: Brendan O'Connor
+# Date: August 2023
+
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip setuptools
+python3 wrapper-build-e2e-pattern-dns.py
