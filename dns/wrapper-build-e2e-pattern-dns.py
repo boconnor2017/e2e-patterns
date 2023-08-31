30731c4ac8c61557229ed2c854b9a3a702f7ce4e
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-08-31 09:08:42 -0400

    Update wrapper-build-e2e-pattern-dns.py

--- dns/wrapper-build-e2e-pattern-dns.py
+++ dns/wrapper-build-e2e-pattern-dns.py
@@ -1,1 +1,49 @@
-#TODO
+# Builds DNS Server using Tanium container
+# Author: Brendan O'Connor
+# Date: August 2023
+
+import os
+import shutil
+
+# Get pattern config file
+src_file = '/usr/local/e2e-patterns/config.py'
+des_dir = os.getcwd()
+print("Retrieving config file from "+src_file+" and copying to "+des_dir)
+outpt = shutil.copy(src_file, des_dir)
+print(str(outpt))
+print("")
+
+# Get pattern library
+src_file = '/usr/local/e2e-patterns/lib.py'
+des_dir = os.getcwd()
+print("Retrieving library file from "+src_file+" and copying to "+des_dir)
+outpt = shutil.copy(src_file, des_dir)
+print(str(outpt))
+print("")
+
+import config
+import lib
+
+# Start log file
+logfile_name = config.LOGS().template
+err = "Starting wrapper-build-e2e-pattern-photon.py"
+lib.write_to_logs(err, logfile_name)
+err = "Input variables:"
+lib.write_to_logs(err, logfile_name)
+i=0 
+for args in sys.argv:
+	err = "    "+args[i]
+	lib.write_to_logs(err, logfile_name)
+	i=i+1
+
+
+#TEST 
+a = "This works!!"
+b = config.IPAM().tag[0] 
+c = config.IPAM().fqdn[0]
+d = config.IPAM().ip[0]
+e = a 
+
+
+print(a)
+print(b+" "+c+" "+d)
