5bbbcfcab00447cb579e5f8fff069123dd8d3605
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-04 09:28:54 -0400

    Update build-e2e-pattern-saltminion.sh

--- salt/build-e2e-pattern-saltminion.sh
+++ salt/build-e2e-pattern-saltminion.sh
@@ -1,3 +1,7 @@
+# Builds a Salt Minion
+# Author: Brendan O'Connor
+# Date: September 2023
+
 python3 -m ensurepip
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade pip setuptools
