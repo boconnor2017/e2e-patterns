0a326777a12b913e8559beb2a6709ec47be4b4bb
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-04 09:18:36 -0400

    Create build-e2e-pattern-saltmaster.sh

---
+++ salt/build-e2e-pattern-saltmaster.sh
@@ -0,0 +1,9 @@
+# Builds a Salt Master
+# Author: Brendan O'Connor
+# Date: September 2023
+
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip setuptools
+curl -L https://bootstrap.saltstack.com -o install_salt.sh
+sh install_salt.sh -M
