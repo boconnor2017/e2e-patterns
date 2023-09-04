75171e30f944b571898c49b01af89d21ba1ec5e6
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-04 09:28:33 -0400

    Update build-e2e-pattern-saltminion.sh

--- salt/build-e2e-pattern-saltminion.sh
+++ salt/build-e2e-pattern-saltminion.sh
@@ -1,2 +1,5 @@
+python3 -m ensurepip
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade pip setuptools
 curl -L https://bootstrap.saltstack.com -o install_salt.sh
 sh install_salt.sh -A <Salt Master IP address>
