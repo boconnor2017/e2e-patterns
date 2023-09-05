edea81260024e15f89ffd12969cf57f984dff573
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-05 11:55:10 -0400

    Update lib.py

--- lib.py
+++ lib.py
@@ -1,6 +1,6 @@
 # E2E Patterns Python Library
 # Author: Brendan O'Connor
-# Date: September 2023
+# Date: September 2023 (Version 2.0)
 
 import requests
 import urllib3
@@ -29,6 +29,8 @@ def write_to_logs(err, logfile_name):
     logfile.write(tstamp+": "+err+" \n")
     logfile.close
 
+# E2E LIBRARY
+
 def build_photon_with_ovftool_container(vm_name, vm_source):
     docker_image = "ovftool" 
     docker_volume = {"/usr/local/drop":{'bind':'/root/home', 'mode':'rw'}}
@@ -47,3 +49,27 @@ def build_photon_with_ovftool_container(vm_name, vm_source):
     err = dclient.containers.run(image=docker_image, volumes=docker_volume, tty=True, working_dir="/root/home", remove=True, command=docker_cmd)
     return str(err)
 
+def get_vm_ip_address(vm_name):
+    docker_rm = True
+    docker_entrypoint = "/usr/bin/pwsh"
+    docker_volume = {os.getcwd():{'bind':'/tmp', 'mode':'rw'}}
+    docker_image = "vmware/powerclicore"
+    docker_cmd = "/tmp/get-vm-ip.ps1 \""+config.E2EP_ENVIRONMENT().esxi_host_ip+" "+config.E2EP_ENVIRONMENT().esxi_host_username+" "+config.E2EP_ENVIRONMENT().esxi_host_password+" "+vm_name+"\""
+    dclient = docker.from_env()
+    ip_address_raw = dclient.containers.run(image=docker_image, entrypoint=docker_entrypoint, volumes=docker_volume, remove=docker_rm, command=docker_cmd)
+    ip_address_raw = str(ip_address_raw)
+    ip_address = ip_address_raw[-17:-5]
+    return ip_address
+
+def change_vm_ip_address(vm_name, new_vm_password):
+    dclient = docker.from_env()
+    docker_entrypoint = "/usr/bin/pwsh"
+    docker_volume = {os.getcwd():{'bind':'/tmp', 'mode':'rw'}}
+    docker_image = "vmware/powerclicore"
+    docker_cmd = "/tmp/configure-photon.ps1 \" "
+    docker_cmd = docker_cmd+config.E2EP_ENVIRONMENT().esxi_host_ip+" "
+    docker_cmd = docker_cmd+config.E2EP_ENVIRONMENT().esxi_host_username+" "
+    docker_cmd = docker_cmd+config.E2EP_ENVIRONMENT().esxi_host_password+" "
+    docker_cmd = docker_cmd+vm_name+" "
+    docker_cmd = docker_cmd+new_vm_password+"\""
+    dclient.containers.run(image=docker_image, entrypoint=docker_entrypoint, volumes=docker_volume, remove=True, command=docker_cmd)
