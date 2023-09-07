d8b1fe55ea80cb431763db86758bbac871428a02
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-07 16:08:28 -0400

    Update run-docker-compose.sh

--- dns/run-docker-compose.sh
+++ dns/run-docker-compose.sh
@@ -1,4 +1,5 @@
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
-docker-compose up &
+mv /usr/local/e2e-patterns/dns/docker-compose.yml $HOME
+docker-compose up -d
