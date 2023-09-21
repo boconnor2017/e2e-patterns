89303f5d3d6bc7abbb1661c6d1c59903db7086c1
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-21 15:42:21 -0700

    Create build-e2ep-template.py

---
+++ template/build-e2ep-template.py
@@ -0,0 +1,95 @@
+# Python template for E2E Patterns
+# Python files should follow this naming convention: build-e2e-pattern-<NAME>.py
+# Copy Template Below
+# # # # # # # # # # # # # # # # # #
+# Description: Installs <SOMETHING INTERESTING> on a Photon controller
+# Author: Brendan O'Connor
+# Date: <MONTH YEAR>
+# Version: <VERSION NUMBER>
+
+# Base imports
+import os
+import shutil
+import sys
+
+# Get pattern config file and library
+des_dir = str(os.getcwd())
+os.chdir("../")
+src_dir = str(os.getcwd())
+os.chdir(des_dir)
+src_file = src_dir+'/config.py'
+outpt = shutil.copy(src_file, des_dir)
+src_file = src_dir+'/lib.py'
+outpt = shutil.copy(src_file, des_dir)
+
+# Import pattern config and library
+import config
+import lib
+
+# Start log file
+logfile_name = config.LOGS().template
+err = ""
+lib.write_to_logs(err, logfile_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
+err = "* * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+lib.write_to_logs(err, logfile_name)
+err = "* * * * * * * * * * * * * * * * * * * * * * * * * * * * *"
+lib.write_to_logs(err, logfile_name)
+err = "Starting "+config.GITHUB().template
+lib.write_to_logs(err, logfile_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
+
+# Check for input parameters
+err = "Checking for input parameters:"
+lib.write_to_logs(err, logfile_name)
+err = "    parameters: "+sys.argv[1]
+lib.write_to_logs(err, logfile_name)
+if sys.argv[1] == "-p":
+    skip_build_photon_controller = True
+    err = "    skipping build photon controller"
+    lib.write_to_logs(err, logfile_name)
+else:
+    skip_build_photon_controller = False
+
+# Photon controller prerequisites
+if skip_build_photon_controller:
+    skip_build_photon_controller = True 
+else:
+    err = "Copying PowerCLI scripts from /photon repo"
+    lib.write_to_logs(err, logfile_name)
+    src_file = src_dir+'/photon/change-photon_default_pw.ps1'
+    outpt = shutil.copy(src_file, des_dir)
+    src_file = src_dir+'/photon/get-vm-ip.ps1'
+    outpt = shutil.copy(src_file, des_dir)
+    src_file = src_dir+'/photon/change-vm-ip.ps1'
+    outpt = shutil.copy(src_file, des_dir)
+    err = ""
+    lib.write_to_logs(err, logfile_name)
+
+# Build photon controller 
+vm_name = config.TEMPLATE().photon_controller_vm_name
+if skip_build_photon_controller:
+    skip_build_photon_controller = True 
+else:
+    err = "Building photon controller."
+    lib.write_to_logs(err, logfile_name)
+    lib.build_photon_controller(vm_name, config.E2EP_ENVIRONMENT().photonos_source, logfile_name)
+    err = ""
+    lib.write_to_logs(err, logfile_name)
+
+# Get VM IP
+err = "Getting IP address of photon controller:"
+lib.write_to_logs(err, logfile_name)
+err = "    vm name: "+vm_name
+lib.write_to_logs(err, logfile_name)
+ip_address = lib.get_vm_ip_address(vm_name)
+err = "    ip address: "+ip_address
+lib.write_to_logs(err, logfile_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
+
+# CUSTOM SCRIPTING FOR THE PATTERN GOES HERE
