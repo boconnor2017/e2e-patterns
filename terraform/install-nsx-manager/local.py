a43b28dffcc269fa32f1bd5d2e26d297437991e1
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-26 14:33:20 -0400

    Create local.py

---
+++ terraform/install-nsx-manager/local.py
@@ -0,0 +1,46 @@
+# Description: local.py for Pattern C-02
+# Author: Brendan O'Connor
+# Date: September 2023
+# Version: 1.0
+
+# Base imports
+import os
+import shutil
+import sys
+
+# Get pattern config file and library
+des_dir = str(os.getcwd())
+os.chdir("../")
+src_dir = str(os.getcwd())
+os.chdir(des_dir)
+src_file = src_dir+'/config.py'
+outpt = shutil.copy(src_file, des_dir)
+src_file = src_dir+'/lib.py'
+outpt = shutil.copy(src_file, des_dir)
+
+# Import pattern config and library
+import config
+import lib
+
+'''
+local.py steps:
+01. Build nsx.tfvars file
+02. Download file from github (main.tf)
+03. Run terraform init command (dir)
+04. Run terraform apply command (dir)
+'''
+
+# Build nsx.tfvars file
+
+# Download main.tf from github
+
+# Copy main.tf to /usr/local/drop
+src_file = config.NSX().nsx_terraform_source
+des_dir = "/usr/local/drop"
+outpt = shutil.copy(src_file, des_dir)
+
+# Run terraform init 
+err = lib.run_terraform_init(des_dir)
+
+# Run terraform apply
+err = lib.run_terraform_apply(des_dir)
