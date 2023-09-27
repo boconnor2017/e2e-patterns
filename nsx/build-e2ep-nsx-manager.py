a5782834e975e529c8a95860b120dbca4bc6f782
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-27 08:09:22 -0400

    Update build-e2ep-nsx-manager.py

--- nsx/build-e2ep-nsx-manager.py
+++ nsx/build-e2ep-nsx-manager.py
@@ -78,4 +78,4 @@ lib.write_to_logs(err, logfile_name)
 # Run Terraform on Photon Controller
 err = "Building NSX with Terraform"
 lib.write_to_logs(err, logfile_name)
-err = lib.run_terraform_on_pattern_controller(ip_address, config.E2EP_ENVIRONMENT().photonos_username, config.E2EP_ENVIRONMENT().photonos_password, config.NSX().main_tf_git_url, config.NSX().local_py_git_url, config.NSX().run_tf_local_dir, logfile_name)
+err = lib.run_terraform_on_pattern_controller(ip_address, config.E2EP_ENVIRONMENT().photonos_username, config.E2EP_ENVIRONMENT().photonos_password, config.NSX().main_tf_git_url, config.NSX().local_py_git_url, config.NSX().main_tf_local_dir, config.NSX().local_py_local_dir, logfile_name)
