759e96f1705733f0d1017657d5c266ef1b2aaf3b
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-27 09:55:26 -0400

    Update build-e2ep-nsx-manager.py

--- nsx/build-e2ep-nsx-manager.py
+++ nsx/build-e2ep-nsx-manager.py
@@ -24,7 +24,7 @@ import lib
 
 # Start log file
 logfile_name = config.LOGS().nsx
-pattern_name = config.TEMPLATE().pattern
+pattern_name = config.NSX().pattern
 lib.e2e_patterns_header(logfile_name, pattern_name)
 err = ""
 lib.write_to_logs(err, logfile_name)
