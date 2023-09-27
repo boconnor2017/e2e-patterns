3fd747d44f9ad3b0846f25b98f1becf2cc9c8d8d
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-27 09:47:42 -0400

    Update local.py

--- terraform/install-nsx-manager/local.py
+++ terraform/install-nsx-manager/local.py
@@ -5,6 +5,7 @@
 
 # Base imports
 import os
+from os.path import exists
 import shutil
 import sys
 
@@ -13,9 +14,9 @@ des_dir = str(os.getcwd())
 os.chdir("../")
 src_dir = str(os.getcwd())
 os.chdir(des_dir)
-src_file = src_dir+'/config.py'
+src_file = src_dir+'/e2e-patterns/config.py'
 outpt = shutil.copy(src_file, des_dir)
-src_file = src_dir+'/lib.py'
+src_file = src_dir+'/e2e-patterns/lib.py'
 outpt = shutil.copy(src_file, des_dir)
 
 # Import pattern config and library
@@ -30,10 +31,43 @@ local.py steps:
 '''
 
 # Build nsx.tfvars file
+tf_var_txt = ["# nsx.tfvars file",
+"variable \"data_center\" { default = \""+config.VCSA().datacenter+"\" }" ,
+"variable \"vds\" { default = \""+config.E2EP_ENVIRONMENT().esxi_host_virtual_switch+"\" }",
+"variable \"workload_datastore\" { default = \""+config.E2EP_ENVIRONMENT().esxi_host_datastore+"\" }",
+"variable \"compute_host\" {default = \""+config.E2EP_ENVIRONMENT().esxi_host_ip+"\"}",
+"variable \"vsphere_server\" {default = \""+config.VCSA().ip+"\"}",
+"variable \"vsphere_user\" { default = \""+config.VCSA().username+"\"}",
+"variable \"vsphere_password\" { default = \""+config.UNIVERSAL().password+"\"}",
+"variable \"mgmt_pg\" { default = \""+config.E2EP_ENVIRONMENT().esxi_host_virtual_switch+"\" }",
+"variable \"vm_name\" { default = \""+config.NSX().nsx_vm_name+"\" }",
+"variable \"local_ovf_path\" { default = \"/usr/local/drop/"+config.NSX().nsx_ova_source+"\" }",
+"variable \"deployment_option\" { default = \""+config.NSX().mgrformfactor+"\" }",
+"variable \"nsx_role\" { default = \"NSX Manager\" }",
+"variable \"nsx_ip_0\" { default = \""+config.NSX().ip+"\" }",
+"variable \"nsx_netmask_0\" { default = \""+config.E2EP_ENVIRONMENT().subnet_mask+"\" }",
+"variable \"nsx_gateway_0\" { default = \""+config.E2EP_ENVIRONMENT().default_gw+"\" }",
+"variable \"nsx_dns1_0\" { default = \""+config.DNS().ip+"\" }",
+"variable \"nsx_domain_0\" { default = \""+config.DNS().zone+"\" }",
+"variable \"nsx_ntp_0\" { default = \""+config.E2EP_ENVIRONMENT().ntp_server+"\" }",
+"variable \"nsx_isSSHEnabled\" { default = \"True\" }",
+"variable \"nsx_allowSSHRootLogin\" { default = \"True\" }",
+"variable \"nsx_passwd_0\" { default = \""+config.NSX().password+"\" }",
+"variable \"nsx_cli_passwd_0\" { default = \""+config.NSX().password+"\" }",
+"variable \"nsx_cli_audit_passwd_0\" { default = \""+config.NSX().password+"\" }",
+"variable \"nsx_hostname\" { default = \""+config.NSX().fqdn+"\" }"
+]
 
+tf_var_file_name = "nsx_var.tf"
+tf_var_file_exists = exists(tf_var_file_name)
+if tf_var_file_exists:
+    os.remove(tf_var_file_name)
+
+for line in tf_var_txt:
+    lib.append_text_to_file(line+"\n", tf_var_file_name)
 
 # Run terraform init 
-err = lib.run_terraform_init(des_dir)
+#err = lib.run_terraform_init(des_dir)
 
 # Run terraform apply
-err = lib.run_terraform_apply(des_dir)
+#err = lib.run_terraform_apply(des_dir)
