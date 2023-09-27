78288fa1f53c8fcc18ccfd24f7415b08aad32c45
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2023-09-27 14:38:23 -0400

    Update build-e2ep-nsx-manager.py

--- nsx/build-e2ep-nsx-manager.py
+++ nsx/build-e2ep-nsx-manager.py
@@ -79,3 +79,17 @@ lib.write_to_logs(err, logfile_name)
 err = "Building NSX with Terraform"
 lib.write_to_logs(err, logfile_name)
 err = lib.run_terraform_on_pattern_controller(ip_address, config.E2EP_ENVIRONMENT().photonos_username, config.E2EP_ENVIRONMENT().photonos_password, config.NSX().main_tf_git_url, config.NSX().local_py_git_url, config.NSX().main_tf_local_dir, config.NSX().local_py_local_dir, logfile_name)
+
+# Create DNS Entry
+err = "Creating DNS Entry:"
+lib.write_to_logs(err, logfile_name)
+token = get_dns_token()
+err = "    token: "+token
+lib.write_to_logs(err, logfile_name)
+err = "    domain hostname: "+config.NSX().domain_hostname
+lib.write_to_logs(err, logfile_name)
+err = "    zone: "+config.DNS().zone
+lib.write_to_logs(err, logfile_name)
+err = "    ip: "+config.DNS().ip
+lib.write_to_logs(err, logfile_name)
+lib.create_dns_record(token, config.NSX().domain_hostname, config.DNS().zone, config.NSX().ip)
