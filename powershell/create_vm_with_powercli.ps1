6573ffe032cc191b5c3c214a953229ae7c0b5346
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-01-16 15:10:48 -0500

    Update create_vm_with_powercli.ps1

--- powershell/create_vm_with_powercli.ps1
+++ powershell/create_vm_with_powercli.ps1
@@ -3,5 +3,6 @@ param(
 [string]$a
 )
 $b = $a.Split(" ")
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
 Connect-VIServer -Server b[1] -Protocol https -User b[2] -Password b[3] -Force
 New-VM -Name b[4]
