86635fff8961555e2f75e98bca848c955312347b
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-01-16 16:07:22 -0500

    Create powe_on_vm_with_powercli.ps1

---
+++ powershell/powe_on_vm_with_powercli.ps1
@@ -0,0 +1,8 @@
+#!/bin/sh
+param(
+[string]$a
+)
+$b = $a.Split(" ")
+Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
+Connect-VIServer -Server $b[0] -Protocol https -User $b[1] -Password $b[2] -Force
+Start-VM -Name $b[3]
