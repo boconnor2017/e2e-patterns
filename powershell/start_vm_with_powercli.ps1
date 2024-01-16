8e92dcb005ae4cc4b13171d09c72aeb7a86be3ce
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-01-16 16:37:27 -0500

    Update start_vm_with_powercli.ps1

--- powershell/start_vm_with_powercli.ps1
+++ powershell/start_vm_with_powercli.ps1
@@ -5,4 +5,4 @@ param(
 $b = $a.Split(" ")
 Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $true
 Connect-VIServer -Server $b[0] -Protocol https -User $b[1] -Password $b[2] -Force
-Start-VM -Name $b[3] -Confirm:$false
+Start-VM -Name $b[3] -Force -Confirm:$false
