f8096d46ef8f9c3bb5f64ec5d8e5678c6c92d358
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-01-17 08:52:07 -0500

    Create lib2024.py

---
+++ lib2024.py
@@ -0,0 +1,20 @@
+# E2E Patterns Python Library 2024
+# Author: Brendan O'Connor
+# Date: January 2024
+# Version: 3.0
+# Reference: https://github.com/boconnor2017/e2e-patterns
+
+import requests
+import urllib3
+import urllib
+import shutil
+import time
+import json
+import os
+import docker 
+import paramiko
+import docker
+import subprocess
+import sys
+from vmware.vapi.vsphere.client import create_vsphere_client
+from datetime import datetime
