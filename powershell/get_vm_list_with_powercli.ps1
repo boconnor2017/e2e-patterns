47b5144c8a17e80acb05883b900709d993782272
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-01-18 13:53:12 -0500

    Create get_vm_list_with_powercli.ps1

---
+++ powershell/get_vm_list_with_powercli.ps1
@@ -0,0 +1,18 @@
+# PowerCLI Script to capture list of virtual machines on an ESXi host
+# Author: Brendan O'Connor
+# Date: January 2024
+
+# Capture input from command line 
+# 0: ESXi Host 
+# 1: ESXi User (root recommended)
+# 2: ESXi Password
+
+param(
+[string]$a
+)
+
+# Connect to ESXi Host
+Connect-VIServer -Server $b[0] -Protocol https -User $b[1] -Password $b[2] -Force
+
+$vmlist = Get-VM | Select Name 
+Write-Host $vmlist  
