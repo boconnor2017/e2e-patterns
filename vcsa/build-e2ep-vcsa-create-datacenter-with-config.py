d55a3d8b9421335620efb3076fa2b9608e0eb9e2
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-01-19 15:43:47 -0500

    Create build-e2ep-vcsa-create-datacenter-with-config.py

---
+++ vcsa/build-e2ep-vcsa-create-datacenter-with-config.py
@@ -0,0 +1,60 @@
+# Description: Creates a datacenter in vCenter using config
+# Author: Brendan O'Connor
+# Date: January 2024
+# Version: 3.0
+
+# lib and config filenames for the year 
+# Must be available in /e2e-patterns repository
+lib_filename = "lib2024.py"
+config_filename = "config2024.py"
+
+# Base imports
+import os
+import shutil
+import sys
+
+# Copy latest lib and config files
+fullpath = os.getcwd()
+dirs = fullpath.split('/')
+count_dirs = len(dirs)
+currdir = dirs[count_dirs-1]
+homedir = dirs[count_dirs-2]
+shell_dir = ""
+i=0 
+for x in dirs:
+    if i == 0:
+        shell_dir = "/"
+        i=i+1
+    if i == (count_dirs-1):
+        shell_dir = shell_dir
+    if i == (count_dirs-2):
+        shell_dir = shell_dir
+    else:
+        shell_dir = shell_dir+dirs[i]+"/"
+        i=i+1
+
+lib_path = shell_dir+homedir+"/"+lib_filename
+config_path = shell_dir+homedir+"/"+config_filename
+shutil.copy(lib_path, fullpath)
+os.rename(lib_filename, "lib.py")
+shutil.copy(config_path, fullpath)
+os.rename(config_filename, "config.py")
+
+# Import pattern config and library
+import config
+import lib
+
+# Start log file
+logfile_name = config.LOGS().vcsa
+pattern_name = config.VCSA().pattern
+lib.e2e_patterns_header(logfile_name, pattern_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
+
+'''
+[1] datacenter name
+'''
+
+# Create datacenter in vCenter
+vcenter_session_id = lib.e2e_get_vcenter_session_id(config.VCSA().fqdn, config.VCSA().username, config.UNIVERSAL().password)
+lib.e2e_create_datacenter_in_vcenter(vcenter_session_id, config.VCSA().fqdn, config.VCSA().datacenter)
