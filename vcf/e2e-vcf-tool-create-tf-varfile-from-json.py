f9f1789e60304db0d5d3f8c0e94cb866f12413a1
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-01-23 16:18:02 -0500

    Update e2e-vcf-tool-create-tf-varfile-from-json.py

--- vcf/e2e-vcf-tool-create-tf-varfile-from-json.py
+++ vcf/e2e-vcf-tool-create-tf-varfile-from-json.py
@@ -1,24 +1,45 @@
-# Description: Create VCF 5 var file for Terraform
+# Description: Parses VCF JSON File and Create TF Var File
 # Author: Brendan O'Connor
-# Date: November 2023
-# Version: 1.0
+# Date: January 2024
+# Version: 2.0
+
+# lib and config filenames for the year 
+# Must be available in /e2e-patterns repository
+lib_filename = "lib2024.py"
+config_filename = "config2024.py"
 
 # Base imports
 import os
 import shutil
-import sys
-import json
+import json 
 from os.path import exists
 
-# Get pattern config file and library
-des_dir = str(os.getcwd())
-os.chdir("../")
-src_dir = str(os.getcwd())
-os.chdir(des_dir)
-src_file = src_dir+'/config.py'
-outpt = shutil.copy(src_file, des_dir)
-src_file = src_dir+'/lib.py'
-outpt = shutil.copy(src_file, des_dir)
+# Copy latest lib and config files
+fullpath = os.getcwd()
+dirs = fullpath.split('/')
+count_dirs = len(dirs)
+currdir = dirs[count_dirs-1]
+homedir = dirs[count_dirs-2]
+shell_dir = ""
+i=0 
+for x in dirs:
+    if i == 0:
+        shell_dir = "/"
+        i=i+1
+    if i == (count_dirs-1):
+        shell_dir = shell_dir
+    if i == (count_dirs-2):
+        shell_dir = shell_dir
+    else:
+        shell_dir = shell_dir+dirs[i]+"/"
+        i=i+1
+
+lib_path = shell_dir+homedir+"/"+lib_filename
+config_path = shell_dir+homedir+"/"+config_filename
+shutil.copy(lib_path, fullpath)
+os.rename(lib_filename, "lib.py")
+shutil.copy(config_path, fullpath)
+os.rename(config_filename, "config.py")
 
 # Import pattern config and library
 import config
@@ -58,4 +79,3 @@ if tf_var_file_exists:
 
 for line in tf_var_txt:
     lib.append_text_to_file(line+"\n", tf_var_file_name)
-
