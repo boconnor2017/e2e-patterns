5c8df119ca0c25708d1dfd911f452fcb7240adf1
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-02-01 16:32:33 -0500

    Update build-e2ep-cloudbuilder.py

--- vcf/build-e2ep-cloudbuilder.py
+++ vcf/build-e2ep-cloudbuilder.py
@@ -53,16 +53,16 @@ lib.write_to_logs(err, logfile_name)
 # 01. Check for node controller
 err = "01. Check for node controller started."
 lib.write_to_logs(err, logfile_name)
-node_controller_exists = lib.e2e_check_for_node_controller(config.VCSA().photon_controller_vm_name)
+node_controller_exists = lib.e2e_check_for_node_controller(config.CLOUD_BUILDER().photon_controller_vm_name)
 if node_controller_exists == 1:
-    err = "    "+config.VCSA().photon_controller_vm_name+" exists."
+    err = "    "+config.CLOUD_BUILDER().photon_controller_vm_name+" exists."
     lib.write_to_logs(err, logfile_name)
 else:
-    err = "    "+config.VCSA().photon_controller_vm_name+" does not exist."
+    err = "    "+config.CLOUD_BUILDER().photon_controller_vm_name+" does not exist."
     lib.write_to_logs(err, logfile_name)
     err = "    Running e2e_build_node_controller()."
     lib.write_to_logs(err, logfile_name)
-    lib.e2e_build_node_controller(config.VCSA().photon_controller_vm_name, logfile_name)
+    lib.e2e_build_node_controller(config.CLOUD_BUILDER().photon_controller_vm_name, logfile_name)
 err = "01. Check for node controller finished."
 lib.write_to_logs(err, logfile_name)
 err = ""
@@ -71,8 +71,8 @@ lib.write_to_logs(err, logfile_name)
 # 02. Get IP Address of the node controller 
 err = "02. Get IP Address of the node controller started."
 lib.write_to_logs(err, logfile_name)
-ip_address = lib.docker_powercli_get_vm_ip_address(config.VCSA().photon_controller_vm_name)
-err = "    IP Address of "+config.VCSA().photon_controller_vm_name+" is "+ip_address
+ip_address = lib.docker_powercli_get_vm_ip_address(config.CLOUD_BUILDER().photon_controller_vm_name)
+err = "    IP Address of "+config.CLOUD_BUILDER().photon_controller_vm_name+" is "+ip_address
 lib.write_to_logs(err, logfile_name)
 err = "02. Get IP Address of the node controller finished."
 lib.write_to_logs(err, logfile_name)
@@ -82,14 +82,21 @@ lib.write_to_logs(err, logfile_name)
 # 03. Prompt User to Upload cloudbuilder ova
 err = "03. Prompt User to Upload cloudbuilder ova started."
 lib.write_to_logs(err, logfile_name)
-if node_controller_exists != 1:
+if node_controller_exists == 0:
     err = "    Prompting user to upload "+config.CLOUD_BUILDER().cb_ova_source+" to "+ip_address
     lib.write_to_logs(err, logfile_name)
+    print("")
+    print("")
+    print("--------------------------------------------------------------")
     print("The script is paused. User input is required.")
     print("Please upload "+config.CLOUD_BUILDER().cb_ova_source+" to "+ip_address+" /usr/local/drop repository.")
+    print("The cloud builder ova is a large file, it is recommended you change the disk size of the node controller to 50GB.")
     print("")
     print("")
     userprompt = input("Hit ENTER key when finished.")
+    print("")
+    print("")
+    print("--------------------------------------------------------------")
     err = "    User prompt: "+userprompt
     lib.write_to_logs(err, logfile_name)    
 err = "03. Prompt User to Upload cloudbuilder ova finished."
@@ -97,4 +104,13 @@ lib.write_to_logs(err, logfile_name)
 err = ""
 lib.write_to_logs(err, logfile_name)
 
-# CUSTOM
+# 04. Build Cloud Builder
+err = "04. Build Cloud Builder started."
+lib.write_to_logs(err, logfile_name)
+err = "    04A. VM Name: "+config.CLOUD_BUILDER().cb_vm_name
+lib.write_to_logs(err, logfile_name)
+lib.docker_powercli_create_cloud_builder(config.CLOUD_BUILDER().cb_vm_name)
+err = "04. Build Cloud Builder finished."
+lib.write_to_logs(err, logfile_name)
+err = ""
+lib.write_to_logs(err, logfile_name)
