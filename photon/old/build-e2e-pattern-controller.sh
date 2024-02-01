91eb1dd5d0243dce43aa654dc88a888345d99c73
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-02-01 15:53:43 -0500

    Delete photon/old/build-e2e-pattern-controller.sh

--- photon/old/build-e2e-pattern-controller.sh
+++
@@ -1,9 +0,0 @@
-# Runs proton prep script remotely from master controller
-# Author: Brendan O'Connor
-# Date: August 2023
-python3 -m ensurepip
-python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade pip setuptools
-python3 -m pip install paramiko
-python3 -m install docker
-python3 wrapper-build-e2e-pattern-controller.py $1 $2
