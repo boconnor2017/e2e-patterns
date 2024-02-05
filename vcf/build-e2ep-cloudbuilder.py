12c53efaa9bca4c9ace2d45b95d863e162f464d5
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2024-02-05 08:35:07 -0500

    Update build-e2ep-cloudbuilder.py

--- vcf/build-e2ep-cloudbuilder.py
+++ vcf/build-e2ep-cloudbuilder.py
@@ -1,7 +1,7 @@
-# Description: Builds CloudBuilder for VCF 5.1
+# Description: Builds CloudBuilder for VCF 5.1 (run from NC)
 # Author: Brendan O'Connor
 # Date: February 2024
-# Version: 2.0
+# Version: 2.1
 
 # lib and config filenames for the year 
 # Must be available in /e2e-patterns repository
@@ -50,75 +50,16 @@ lib.e2e_patterns_header(logfile_name, pattern_name)
 err = ""
 lib.write_to_logs(err, logfile_name)
 
-# 01. Check for node controller
-err = "01. Check for node controller started."
+# 01. Build Cloud Builder
+err = "01. Build Cloud Builder started."
 lib.write_to_logs(err, logfile_name)
-node_controller_exists = lib.e2e_check_for_node_controller(config.CLOUD_BUILDER().photon_controller_vm_name)
-if node_controller_exists == 1:
-    err = "    "+config.CLOUD_BUILDER().photon_controller_vm_name+" exists."
-    lib.write_to_logs(err, logfile_name)
-else:
-    err = "    "+config.CLOUD_BUILDER().photon_controller_vm_name+" does not exist."
-    lib.write_to_logs(err, logfile_name)
-    err = "    Running e2e_build_node_controller()."
-    lib.write_to_logs(err, logfile_name)
-    lib.e2e_build_node_controller(config.CLOUD_BUILDER().photon_controller_vm_name, logfile_name)
-err = "01. Check for node controller finished."
-lib.write_to_logs(err, logfile_name)
-err = ""
-lib.write_to_logs(err, logfile_name)
-
-# 02. Get IP Address of the node controller 
-err = "02. Get IP Address of the node controller started."
-lib.write_to_logs(err, logfile_name)
-ip_address = lib.docker_powercli_get_vm_ip_address(config.CLOUD_BUILDER().photon_controller_vm_name)
-err = "    IP Address of "+config.CLOUD_BUILDER().photon_controller_vm_name+" is "+ip_address
-lib.write_to_logs(err, logfile_name)
-err = "02. Get IP Address of the node controller finished."
-lib.write_to_logs(err, logfile_name)
-err = ""
-lib.write_to_logs(err, logfile_name)
-
-# 03. Prompt User to Upload cloudbuilder ova
-if node_controller_exists == 0:
-    err = "03. Prompt User to Upload cloudbuilder ova started."
-    lib.write_to_logs(err, logfile_name)
-    err = "    Prompting user to upload "+config.CLOUD_BUILDER().cb_ova_source+" to "+ip_address
-    lib.write_to_logs(err, logfile_name)
-    print("")
-    print("")
-    print("--------------------------------------------------------------")
-    print("The script is paused. User input is required.")
-    print("Please upload "+config.CLOUD_BUILDER().cb_ova_source+" to "+ip_address+" /usr/local/drop repository.")
-    print("The cloud builder ova is a large file, it is recommended you change the disk size of the node controller to 50GB.")
-    print("")
-    print("")
-    userprompt = input("Hit ENTER key when finished.")
-    print("")
-    print("")
-    print("--------------------------------------------------------------")
-    err = "    User prompt: "+userprompt
-    lib.write_to_logs(err, logfile_name)    
-    err = "03. Prompt User to Upload cloudbuilder ova finished."
-    lib.write_to_logs(err, logfile_name)
-    err = ""
-    lib.write_to_logs(err, logfile_name)
-else:
-    err = "03. Skipping step 3. Prompt not needed."
-    lib.write_to_logs(err, logfile_name)
-    err = ""
-    lib.write_to_logs(err, logfile_name)
-
-# 04. Build Cloud Builder
-err = "04. Build Cloud Builder started."
-lib.write_to_logs(err, logfile_name)
-err = "    04A. VM Name: "+config.CLOUD_BUILDER().cb_vm_name
+err = "    01A. VM Name: "+config.CLOUD_BUILDER().cb_vm_name
 lib.write_to_logs(err, logfile_name)
 logfile_name = fullpath+"/"+logfile_name
-err = "    04B. Specifying proper logfile directory: "+logfile_name
+err = "    01B. Specifying proper logfile directory: "+logfile_name
 lib.write_to_logs(err, logfile_name)
 lib.docker_powercli_create_cloud_builder(config.CLOUD_BUILDER().cb_vm_name)
-err = "04. Build Cloud Builder finished."
+err = "01. Build Cloud Builder finished."
 lib.write_to_logs(err, logfile_name)
 err = ""
 lib.write_to_logs(err, logfile_name)
